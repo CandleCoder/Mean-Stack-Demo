5186da35207f64d08d867318582205ef1ea963fc
Manikant Upadhyay <manikant.upadhyay@magicsw.com>
2016-09-13 14:12:56 +0530

    adding login api

--- index.js
+++ index.js
@@ -11,6 +11,13 @@ var app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(methodOverride('X-HTTP-Method-Override'));
+app.models = require('./models/index');
+
+// Load the routes.
+var routes = require('./routes');
+_.each(routes, function(controller, route) {
+  app.use(route, controller(app, route));
+});
 
 // app.use(function(req, res, next) {
 //   res.header('Access-Control-Allow-Origin', '*');
@@ -27,7 +34,9 @@ app.use('/login', function(req, res, next) {
 // Connect to MongoDB
 mongoose.connect('mongodb://localhost/meanapp');
 mongoose.connection.once('open', function() {
-								
-  console.log('Listening on port 3000...');
+  console.log('Listening on port 3000...');	  
+  // Load the models.
+  app.models = require('./models/index');								
+
   app.listen(3000);
 });
---
+++ routes.js
@@ -0,0 +1,3 @@
+module.exports = {
+  'login': require('./controllers/LoginController')
+};
